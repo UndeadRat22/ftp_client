a48c37c28a19e94f132ff50fa0f865ec1c7d0488
Valdas Rakutis <valdas@rakutis.lt>
2019-03-25 20:59:09 +0200

    create a client interface.

--- FtpClient/src/org/deadrat/Main.java
+++ FtpClient/src/org/deadrat/Main.java
@@ -1,39 +1,72 @@
 package org.deadrat;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
         var client = new FTPClient(1024);
+        Scanner reader = new Scanner(System.in);
+        while (true)
+        {
+            System.out.print("ftp > ");
+            String line = reader.nextLine();
 
-        try {
-            client.setAddress("127.0.0.1", "21");           
-            client.open();
-        } catch (Exception e) {
-            System.out.println("failed in open");
-        }
-        try {
-            client.login("anonymous", "?");
-        } catch (Exception e) {
-            System.out.println("failed in user");
-        }
-        try {
-            client.getDataPort();
-        }
-        catch (Exception e)
-        {
-            System.out.println("failed to get data port");
-        }
-        try {
-            client.list();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        try {
-            client.getFile("unknown.png", "ppp.png");
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
+            if (line.contains("open"))
+            {
+                try
+                {
+                    var cmdargs = line.split(" ");
+                    if (cmdargs[1].equals("localhost"))
+                    {
+                        client.setAddress("127.0.0.1", "21");
+                    }
+                    else
+                    {
+                        client.setAddress(cmdargs[1], cmdargs[2]);
+                    }
+                    client.open();
+                } catch (Exception e)
+                {
+                    System.err.println("Exception occured when trying to open a ftp connection.");
+                    System.err.println(e);
+                }
+            }
+            else if (line.contains("login"))
+            {
+                try
+                {
+                    var cmdargs = line.split(" ");
+                    client.login(cmdargs[1], cmdargs[2]);
+                } catch (Exception e)
+                {
+                    System.err.println("Exception occured when trying to login to a remote ftp connection.");
+                    System.err.println(e);
+                }
+            }
+            else if (line.substring(0, 3).equals("dir") || line.substring(0, 2).equals("ls"))
+            {
+                try
+                {
+                    client.list();
+                } catch (Exception e)
+                {
+                    System.err.println("Exception occured when trying to list remote files.");
+                    System.err.println(e);
+                }
+            }
+            else if (line.substring(0, 3).equals("get") || line.substring(0, 8).equals("download"))
+            {
+                var cmdargs = line.split(" ");
+                try
+                {
+                    client.getFile(cmdargs[1], cmdargs[2]);
+                } catch (Exception e)
+                {
+                    System.err.println("Exception occured when trying to get remote file " + cmdargs[1] + ".");
+                    System.err.println(e);
+                }
+            }
         }
     }
 }
